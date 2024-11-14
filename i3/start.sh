667e5b92616ec5b6aa2555507cbcd83b1cc8e83b
LucianCrainic <crainic.lucian@gmail.com>
2024-11-14 21:32:04 +0100

    [UPDATE] i3 config colorscheme

--- i3/start.sh
+++ i3/start.sh
@@ -2,9 +2,9 @@
 # xrandr --output DP-1-4-2 --primary --mode 1920x1080
 # xrandr --output DP-1-4-1 --mode 1920x1080 --right-of DP-1-4-2
 # xrandr --output DP-1-4-3 --mode 1920x1080 --left-of DP-1-4-2
-xrandr --output HDMI-1-2 --primary --mode 1920x1080 -r 100.000
-xrandr --output DP-1-4 --mode 1920x1080 --left-of HDMI-1-2 -r 75.00
-feh --bg-fill /home/ldcrainic/.config/wallpapers/background.png --bg-fill /home/ldcrainic/.config/wallpapers/background.png
+xrandr --output HDMI-0 --primary --mode 1920x1080 -r 100.000
+xrandr --output DP-4 --mode 1920x1080 --rotate left --left-of HDMI-0 -r 75.00
+feh --bg-fill /home/lucian/.config/wallpapers/background.png --bg-fill /home/lucian/.config/wallpapers/background.png
 # Turn Off Screen going to sleep
 xset s off
 xset -dpms
