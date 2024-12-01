6041af8db85fd1f49597fa300c80353d899e8079
LucianCrainic <crainic.lucian@gmail.com>
2024-12-01 23:40:29 +0100

    [ADD] harpoon and noice plugins

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -1,23 +1,20 @@
----@type LazySpec
 return {
   "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      diagnostics_mode = 1, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = false, -- highlight URLs at start
+      notifications = false,
     },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+  
     diagnostics = {
       virtual_text = true,
       underline = true,
     },
-    -- vim options can be configured here
+   
     options = {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
@@ -29,8 +26,7 @@ return {
       g = { -- vim.g.<key>
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    
     mappings = {
       -- first key is the mode
       n = {
@@ -50,12 +46,6 @@ return {
           desc = "Close buffer from tabline",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
       },
     },
   },
