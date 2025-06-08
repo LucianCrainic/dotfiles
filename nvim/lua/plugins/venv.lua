79cb447330ee977511c1aad0329db2da95857bf5
LucianCrainic <crainic.lucian@gmail.com>
2025-06-08 15:33:02 +0200

    refactor venv.lua: clean up comments and remove unused keys

--- nvim/lua/plugins/venv.lua
+++ nvim/lua/plugins/venv.lua
@@ -2,11 +2,9 @@ return {
   'linux-cultist/venv-selector.nvim',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
   opts = {
-    },
     auto_refresh = true,
     search_venv_managers = true,
     search_workspace = true,
-    -- Set default paths where venv-selector should look for virtual environments
     path = {
       "~/miniconda3/envs",
       "~/.pyenv/versions",
@@ -16,12 +14,8 @@ return {
       "~/venv",
       "~/.venv",
     },
-    -- Automatically activate the detected virtual environment
     activate_venv_in_terminal = true,
-    -- Set Python path automatically when switching venvs
     set_environment_variables = true,
   },
   lazy = false,
-  keys = {
-  },
 }
