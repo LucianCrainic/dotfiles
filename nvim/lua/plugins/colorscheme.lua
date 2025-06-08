a220b55567c654259b6bf15c8014099855dac861
LucianCrainic <crainic.lucian@gmail.com>
2025-06-08 11:55:13 +0200

    [UPDATE] change colorscheme to Catppuccin and update configuration

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,56 @@
-return { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "frappe",
+      },
+      transparent_background = false,
+      show_end_of_buffer = false,
+      term_colors = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        telescope = {
+          enabled = true,
+        },
+        lsp_trouble = true,
+        which_key = true,
+      },
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
+}
